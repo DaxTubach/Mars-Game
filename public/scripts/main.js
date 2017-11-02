95df4be2a734e8fe55a20833a46d30cae17dd3ec
Damian <damianthesuski@gmail.com>
2017-11-01 23:09:10 -0400

    Pixi files

---
+++ public/scripts/main.js
@@ -0,0 +1,82 @@
+var type = "WebGL"
+var stage;
+var renderer;
+var sprites = [];
+
+function initialize(){
+	if(!PIXI.utils.isWebGLSupported()){
+		type = "canvas";
+	}
+
+	/*This is PIXI JS Version*/
+	PIXI.utils.sayHello(type);
+
+	/*Create renderer*/
+	renderer = PIXI.autoDetectRenderer(256, 256,{antialias:true, transparent: false, resolution: 1});
+
+	/*Add canvas to HTML doc*/
+	document.body.appendChild(renderer.view);
+
+	/*Create stage*/
+	stage = new PIXI.Container();
+
+	renderer.view.style.position = "absolute";
+	renderer.view.style.display = "block";
+	renderer.autoResize = true;
+	renderer.resize(window.innerWidth, window.innerHeight);
+
+	renderer.backgroundColor = 0xFF8C00;
+
+	renderer.render(stage);
+
+    var sound = new Howl({
+    	src:['music/Dust.mp3'],
+		html5: true,
+		loop: true
+	});
+
+    sound.play();
+
+    loadImages();
+}
+
+function loadImages(){
+	PIXI.loader
+	.add("images/astronaut.png")
+	.add("images/plant.png")
+	.add("images/rover.png")
+	.add("images/water-tank.png")
+	.load(setupWorld);
+}
+
+function setupWorld(){
+	var spriteArray = populateWorld();
+	var arrayLength = spriteArray.length;
+
+	sprites = spriteArray;
+
+	for(var i=0; i<arrayLength;i++){
+		stage.addChild(spriteArray[i]);
+	}
+
+	gameLoop();
+}
+
+function gameLoop(){
+	requestAnimationFrame(gameLoop);
+	renderer.render(stage);
+
+	const x = renderer.plugins.interaction.mouse.global.x;
+	const y = renderer.plugins.interaction.mouse.global.y;
+
+	if(Math.abs(sprites[0].x-x) <= 2 && Math.abs(sprites[0].y-y) <= 2)
+		return;
+
+	sprites[0].rotation = Math.atan2(y - sprites[0].y, x - sprites[0].x) + Math.PI;
+
+	sprites[0].x -= Math.cos(sprites[0].rotation);
+	sprites[0].y -= Math.sin(sprites[0].rotation);
+}
+
+initialize();
+

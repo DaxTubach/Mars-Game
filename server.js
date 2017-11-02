c4ce60ac1e2d4949a9c1e23f129eeeaac6c7a6d8
Damian <damianthesuski@gmail.com>
2017-11-01 23:11:47 -0400

    added server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-// server.js
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.set('port',8080);
+
+app.use(express.static(path.join(__dirname,'public')));
+
+var server = app.listen(app.get('port'),function(){
+	var port = server.address().port;
+	console.log('Server operational ' + port);
+});

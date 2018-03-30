6d9a53cbfcce3f65260fc08d5758ba5009d07227
Dax <Daxtubach@gmail.com>
2018-03-29 20:06:37 -0400

    Database update

---
+++ public/auth.js
@@ -0,0 +1,105 @@
+// Initialize Firebase
+
+let config = {
+    apiKey: 'AIzaSyBBFrScSC__f_I53oamxNZTVpLZYmrmlfI',
+    authDomain: 'mars-hq.firebaseapp.com',
+    databaseURL: 'https://mars-hq.firebaseio.com',
+    projectId: 'mars-hq',
+    storageBucket: 'mars-hq.appspot.com',
+    messagingSenderId: '521104559480'
+};
+firebase.initializeApp(config);
+let db = firebase.firestore();
+
+
+function initApp() {
+    // Listening for auth state changes.
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            // User is signed in.
+            const docRef = db.collection('users').doc(user.uid);
+            docRef.get().then((doc) => {
+                if (doc.exists) {
+                    console.log('Document data:', doc.data());
+                    if (doc.data().conlony == null && window.location.pathname != '/colony-selection.html') {
+                        window.location.href = '/colony-selection.html';
+                    }
+                    if (doc.data().conlony != null && window.location.pathname != '/colony-equipment.html')
+                    {
+                        //window.location.href = '/colony-equipment.html';
+                    }
+                    if (doc.data().conlony != null && window.location.pathname != '/colony-equipment.html')
+                    {
+                        //window.location.href = '/location-selection.html';
+                    }
+                    if(doc.data().conlony != null && window.location.pathname != '/colony-equipment.html')
+                    {
+                        //post('/start', {name: 'Tests'});
+                    }
+
+                } else {
+                    // Doc.data() will be undefined in this case
+                    console.log('No such document!');
+                }
+            })
+                .catch((error) => {
+                    console.log('Error getting document:', error);
+                });
+            
+            /*
+            document.getElementById('native-sign-in-status').textContent = 'Signed in';
+            document.getElementById('native-sign-in').textContent = 'Sign out';
+            document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+            */
+
+            // Document.getElementById('google-signin').disabled = true;
+        } else {
+            // User is signed out.
+               // Document.getElementById('google-signin').disabled = false;
+            
+            if(window.location.pathname == '/index.html' || window.location.pathname == '/' )
+            {}
+            else
+                window.location.href = '/index.html';
+       
+            
+            /*
+            document.getElementById('native-sign-in-status').textContent = 'Signed out';
+            document.getElementById('native-sign-in').textContent = 'Sign in';
+            document.getElementById('quickstart-account-details').textContent = 'null';
+            */
+        }
+
+       // document.getElementById('native-sign-in').disabled = false;
+    });
+    // RAF, use these ID names when creating verify email and password reset buttons, I think omission of google sign in for now
+    /*
+        Document.getElementById('verify-email').addEventListener('click', sendEmailVerification, false);
+        document.getElementById('password-reset').addEventListener('click', sendPasswordReset, false);
+        document.getElementById('google-signin').addEventListener('click', toggleGoogleSignIn, false);
+        */
+}
+
+function post(path, params, method) {
+    method = method || "post"; // Set method to post by default if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,44 @@
-var type = "WebGL"
-var stage;
-var renderer;
-var spriteContainer;
-var offsetVX, offsetVY, speedModifier, zoomDelta, zoom;
+let type = 'WebGL';
+let stage;
+let renderer;
+let sprites = [];
 
+const awsE = 'https://s3.us-east-2.amazonaws.com/hq.mars/Entities/';
 
-let awsE = "https://s3.us-east-2.amazonaws.com/hq.mars/Entities/";
+function initialize() {
+    if (!PIXI.utils.isWebGLSupported()) {
+        type = 'canvas';
+    }
 
-function initialize(){
-	if(!PIXI.utils.isWebGLSupported()){
-		type = "canvas";
-	}
+    /* This is PIXI JS Version*/
+    PIXI.utils.sayHello(type);
 
-	/*This is PIXI JS Version*/
-	PIXI.utils.sayHello(type);
+    /* Create renderer*/
+    renderer = PIXI.autoDetectRenderer(256, 256, {
+        antialias: true,
+        transparent: false,
+        resolution: 1
+    });
 
-	/*Create renderer*/
-	renderer = PIXI.autoDetectRenderer(256, 256,{antialias:true, transparent: false, resolution: 1});
+    /* Add canvas to HTML doc*/
+    document.body.appendChild(renderer.view);
 
-	/*Add canvas to HTML doc*/
-	document.body.appendChild(renderer.view);
+    /* Create stage*/
+    stage = new PIXI.Container();
 
-	/*Create stage*/
-	stage = new PIXI.Container();
+    renderer.view.style.position = 'absolute';
+    renderer.view.style.display = 'block';
+    renderer.autoResize = true;
+    renderer.resize(window.innerWidth, window.innerHeight);
 
-	renderer.view.style.position = "absolute";
-	renderer.view.style.display = "block";
-	renderer.autoResize = true;
-	renderer.resize(window.innerWidth, window.innerHeight);
-
-	renderer.backgroundColor = 0xFF8C00;
+    renderer.backgroundColor = 0xFF8C00;
 
 	//document.addEventListener("mousewheel",mouseWheelHandler, false);
 
 	renderer.render(stage);
 
-	/*
-    var sound = new Howl({
+    /*
+    Var sound = new Howl({
     	src:['music/Dust.mp3'],
 		html5: true,
 		loop: true
@@ -49,20 +51,20 @@ function initialize(){
     loadImages();
 }
 
-function loadImages(){
-	PIXI.loader
-	.add(awsE+"astronaut.png")
-	.add(awsE+"plant.png")
-	.add(awsE+"rover.png")
-	.add(awsE+"water-tank.png")
-	.load(setupWorld);
+function loadImages() {
+    PIXI.loader
+        .add(`${awsE  }astronaut.png`)
+        .add(`${awsE  }plant.png`)
+        .add(`${awsE  }rover.png`)
+        .add(`${awsE  }water-tank.png`)
+        .load(setupWorld);
 }
 
 function setupWorld(){
 	spriteContainer = populateWorld();
 	stage.addChild(spriteContainer);
 
-	gameLoop();
+    gameLoop();
 }
 
 function gameLoop(){
@@ -89,14 +91,12 @@ function gameLoop(){
 	/*const x = renderer.plugins.interaction.mouse.global.x;
 	const y = renderer.plugins.interaction.mouse.global.y;
 
-	if(Math.abs(sprites[0].x-x) <= 2 && Math.abs(sprites[0].y-y) <= 2)
-		return;
+    if (Math.abs(sprites[0].x - x) <= 2 && Math.abs(sprites[0].y - y) <= 2) return;
 
-	sprites[0].rotation = Math.atan2(y - sprites[0].y, x - sprites[0].x) + Math.PI;
+    sprites[0].rotation = Math.atan2(y - sprites[0].y, x - sprites[0].x) + Math.PI;
 
 	sprites[0].x -= Math.cos(sprites[0].rotation);
 	sprites[0].y -= Math.sin(sprites[0].rotation);*/
 }
 
 initialize();
-
